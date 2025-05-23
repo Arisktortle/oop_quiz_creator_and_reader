cd332e6c24bd83ec2e555ff6d7fbce4b68f19799
Synch Coding <dhdbbdhd161@gmail.com>
2025-05-23 13:57:03 +0800

    function to prompt the user to input question and the choices

--- python_oop_quiz_creator_and_reader.py
+++ python_oop_quiz_creator_and_reader.py
@@ -1,4 +1,14 @@
 #prompt the user for question and choices
+def user_questions(number): 
+    print(f"For question {number}")
+    question = input("Enter your question:\n ").strip() #asks the user for questions
+    
+    print("\n Enter the choices: ") #prompts the user to enter choices
+    choices = {}
+    for option in ['a', 'b', 'c', 'd']: #chooses questions between a, b, c, and d
+        answer = input(f" {option}) ").strip()
+        choices[option] = answer
+        
 #export the questions inputted into a file
 #reads how many questions inputted
 #welcome menu for quiz and calls all three functions
